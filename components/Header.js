c0e133457720a669810f059a91864b643cb4245e
Brandon Rainey <brandonhrainey@gmail.com>
2022-06-15 02:45:56 -0400

    added working auth

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { AiFillHome, AiOutlinePlus } from 'react-icons/ai'
 import { FiChevronDown, FiBell } from 'react-icons/fi'
@@ -11,8 +11,13 @@ import {
 } from 'react-icons/bs'
 import { BiVideoRecording } from 'react-icons/bi'
 import { IoLogoReddit } from 'react-icons/io'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Header() {
+  const {data : session} = useSession()
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="flex border-b h-14 pt-2 pb-2 w-full">
       <div className="w-14 h-14 pl-2 pr-2">
@@ -69,10 +74,15 @@ export default function Header() {
         </div>
       </div>
 
-      <div className="flex w-40 border border-gray-200 rounded justify-center items-center font-semibold mr-2 gap-2 cursor-pointer">
+      <div className="flex w-40 border border-gray-200 rounded justify-center items-center font-semibold mr-2 gap-2 cursor-pointer relative" onClick={() => setOpen(!open)}>
         <IoLogoReddit className="w-6 h-full hidden sm:inline" />
         <FiChevronDown />
       </div>
+      {open ? (<div className='absolute border rounded bg-gray-600 right-2 top-12 w-48 h-96'>
+        {session ? (<div className='bg-blue-400' onClick={() => signOut()}>Sign Out</div>) : (<div className='bg-blue-400' onClick={() => signIn()}>Sign In</div>)}
+        
+      </div>) : null}
+      
     </div>
   )
 }
